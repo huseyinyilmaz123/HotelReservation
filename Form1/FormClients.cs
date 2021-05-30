b9ab2ceb08c3ad5139c698537cf3b18c03ee0ec6
burkayatar5 <burkayatar5@gmail.com>
2021-05-30 14:50:33 +0300

    Add files via upload

--- Form1/FormClients.cs
+++ Form1/FormClients.cs
@@ -15,6 +15,30 @@ namespace Form1
         public FormClients()
         {
             InitializeComponent();
+            timer1.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormMainPage fmp = new FormMainPage();
+            fmp.Show();
+        }
+
+        private void FormClients_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            this.labelTime.Text = dateTime.ToString();
         }
     }
 }
--- Form1/FormLogin.cs
+++ Form1/FormLogin.cs
@@ -1,15 +1,14 @@
-﻿using Business.Concrete;
-using DataAccess.Concrete.EntityFramework;
-using Entities.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Form1
 {
@@ -20,40 +19,34 @@ namespace Form1
             InitializeComponent();
         }
 
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtUserName.Text))
-            {
-                MessageBox.Show("Please enter your username.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                txtUserName.Focus();
-                return;
-            }
-            try
-            {
-                using (HotelContext context = new HotelContext())
-                {
-                    //using linq to query data
-                    var query = from o in context.USERS
-                                where o.UserName == txtUserName.Text && o.Password == txtPassword.Text
-                                select o;
-                    //check if user exists
-                    if (query.SingleOrDefault() != null)
-                    {
-                        MessageBox.Show("You have been successfully logged in.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        //Add your code process login here
-                        FormClients formClients = new FormClients();
-                        formClients.Show();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Your username or password is incorrect.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //   string ad = textBoxUsername.Text;
+            //   string parola = textBoxPassword.Text;
+
+            this.Hide();
+            FormMainPage fmp = new FormMainPage();
+            fmp.Show();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
 
         }
     }
